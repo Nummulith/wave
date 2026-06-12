bc312014a20f005e40e5cfbf3b1302d14f73980f
Nummulith <144725587+Nummulith@users.noreply.github.com>
2026-06-12 18:34:28 +0300

    class Wave for lossless

--- 6lossless.py
+++ 6lossless.py
@@ -1,61 +1,27 @@
 import numpy as np
 from scipy.io import wavfile
-
-def lossless(file_name):
-    # 1. Чтение WAV
-    sou_name = f"source/{file_name}.wav"
-    sample_rate, data = wavfile.read(sou_name)
-    data_float = data.astype(np.float64)
-    print(f"file read: {sou_name} - sample_rate = {sample_rate}")
-
-    # Если моно, делаем (N,1)
-    if data_float.ndim == 1:
-        data_float = data_float[:, np.newaxis]
-    num_channels = data_float.shape[1]
-
-    # format
-    csv_format = False
-
-    # 2. FFT и сохранение каждого канала в отдельный CSV
-    for ch in range(num_channels):
-        fft_data = np.fft.fft(data_float[:, ch])
-        if not csv_format:
-            fft_name = f"fft/{file_name}_ch{ch}.npy"
-            np.save(fft_name, fft_data)
-        else:
-            fft_name = f"fft/{file_name}_ch{ch}.csv"
-            np.savetxt(fft_name, np.column_stack((fft_data.real, fft_data.imag)), delimiter=',', fmt='%.16f')
-        print(f"Channel {ch} FFT saved to: {fft_name}")
-
-    # 3. Восстановление WAV
-    recovered = np.zeros_like(data_float)
-    for ch in range(num_channels):
-        if not csv_format:
-            fft_name = f"fft/{file_name}_ch{ch}.npy"
-            fft_complex = np.load(fft_name)
-        else:
-            fft_name = f"fft/{file_name}_ch{ch}.csv"
-            fft_loaded = np.loadtxt(fft_name, delimiter=',')
-            fft_complex = fft_loaded[:, 0] + 1j * fft_loaded[:, 1]
-
-        recovered[:, ch] = np.fft.ifft(fft_complex).real
-        print(f"Channel {ch} FFT recovered from: {fft_name}")
-
-    # Округляем и возвращаем исходный тип данных
-    recovered = np.round(recovered).astype(data.dtype)
-
-    # Если был моно, убираем лишнюю ось
-    if num_channels == 1:
-        recovered = recovered[:, 0]
-
-    # 4. Проверка
-    diff_check = np.max(np.abs(data - recovered))
-    print("Difference check: max = ", diff_check)
-
-    # 5. Сохранение WAV
-    rec_name = f"recovered/{file_name}.wav"
-    wavfile.write(rec_name, sample_rate, recovered)
-    print(f"Recovered file written: {rec_name}")
+from wave_lib import *
 
 file_name = 'dist_bass'
-lossless(file_name)
+
+sou_name = f"source/{file_name}.wav"
+
+source = Wave()
+source.load_wav(sou_name)
+
+csv_format = False
+
+fft_name = f"fft/{file_name}_ch[ch].{'npy' if not csv_format else 'csv'}"
+saver = source.save_fft if not csv_format else source.save_csv
+saver(fft_name)
+
+dest = Wave()
+dest.copy_properties_from(source)
+
+loader = dest.load_fft if not csv_format else dest.load_csv
+loader(fft_name)
+
+source.diff_check(dest)
+
+rec_name = f"recovered/{file_name}.wav"
+dest.save_wav(rec_name)
---
+++ wave_lib.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.io import wavfile
+
+class Wave:
+    def load_wav(self, file_name):
+        self.sample_rate, data = wavfile.read(file_name)
+        self.data_float = data.astype(np.float64)
+        print(f"file read: {file_name} - sample_rate = {self.sample_rate}")
+
+        # Если моно, делаем (N,1)
+        if self.data_float.ndim == 1:
+            self.data_float = self.data_float[:, np.newaxis]
+        self.num_channels = self.data_float.shape[1]
+        self.dtype = data.dtype
+
+    def file_name_ch(self, file_name, ch):
+        return file_name.replace("[ch]", str(ch))
+
+    def save_fft(self, file_name):
+        for ch in range(self.num_channels):
+            fft_data = np.fft.fft(self.data_float[:, ch])
+            np.save(
+                self.file_name_ch(file_name, ch),
+                fft_data
+            )
+
+    def save_csv(self, file_name):
+        for ch in range(self.num_channels):
+            fft_data = np.fft.fft(self.data_float[:, ch])
+            np.savetxt(
+                self.file_name_ch(file_name, ch),
+                np.column_stack((fft_data.real, fft_data.imag)), delimiter=',', fmt='%.16f'
+            )
+
+    def copy_properties_from(self, source):
+        self.data_float = np.zeros_like(source.data_float)
+        self.num_channels = source.num_channels
+        self.dtype = source.dtype
+        self.sample_rate = source.sample_rate
+
+    def diff_check(self, source):
+        check = np.max(np.abs(self.data_float - source.data_float))
+        print("Difference check: max = ", check)
+
+    def load_fft(self, file_name):
+        for ch in range(self.num_channels):
+            fft_complex = np.load(self.file_name_ch(file_name, ch))
+            self.data_float[:, ch] = np.fft.ifft(fft_complex).real
+
+    def load_csv(self, file_name):
+        for ch in range(self.num_channels):
+            fft_loaded = np.loadtxt(self.file_name_ch(file_name, ch), delimiter=',')
+            fft_complex = fft_loaded[:, 0] + 1j * fft_loaded[:, 1]
+            self.data_float[:, ch] = np.fft.ifft(fft_complex).real
+
+    def save_wav(self, file_name):
+        data = np.round(self.data_float).astype(self.dtype) # Округляем и возвращаем исходный тип данных
+
+        if self.num_channels == 1: # Если был моно, убираем лишнюю ось
+            data = data[:, 0]
+
+        wavfile.write(file_name, self.sample_rate, data)
